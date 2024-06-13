0f0edd4e590c805572de7655695288411071b111
Branden Basche <brandenbasche@gmail.com>
2024-06-13 15:33:19 -0400

    more card functionality and styling, troubleshooting webpack 500 error

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,13 @@
+'use client';
+
 import { cn } from "@/utils/cn";
 import {BackgroundGradientAnimation} from "@/components/ui/GradientBg";
+import {GlobeDemo} from "@/components/ui/GridGlobe";
+import {useState} from "react";
+import animationData from '@/data/confetti.json';
+import Lottie from "react-lottie";
+import MagicButton from "@/components/ui/MagicButton";
+import {IoCopyOutline} from "react-icons/io5";
 
 export const BentoGrid = ({
                               className,
@@ -39,10 +47,21 @@ export const BentoGridItem = ({
         titleClassName?: string;
         spareImg?: string;
 }) => {
+
+    const leftList = ['React.js', 'TypeScript', 'Next.js'];
+    const rightList = ['AWS', 'MongoDB', 'Express.js'];
+
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText('brandenbasche@gmail.com');
+        setCopied(true);
+    }
+
     return (
         <div
             className={cn(
-                "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+                "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
                 className
             )}
             style={{
@@ -84,7 +103,59 @@ export const BentoGridItem = ({
                     </div>
                     <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
                         {title}
+                </div>
+
+                {id === 2 && (
+                    <GlobeDemo />
+                )}
+
+                {id === 3 && (
+                    <div className='flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2'>
+                        <div className='flex flex-col gap-3 lg:gap-8'>
+                            {/*front end tools*/}
+                            {leftList.map((item) => (
+                                <span key={item}
+                                      className='py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]'>
+                                    {item}
+                                </span>
+                            ))}
+                            <span className='py-4 px-3 rounded-lg text-center bg-[#10132e]'/>
+                        </div>
+                        <div className='flex flex-col gap-3 lg:gap-8'>
+                            <span className='py-4 px-3 rounded-lg text-center bg-[#10132e]'/>
+                            {/*back end tools*/}
+                            {rightList.map((item) => (
+                                <span key={item}
+                                      className='py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]'>
+                                    {item}
+                                </span>
+                            ))}
+                        </div>
                     </div>
+                )}
+
+                    {id === 6 && (
+                        <div className='mt-5 relative'>
+                            <div className={`absolute -bottom-5 right-0`}>
+                                <Lottie options={{
+                                    loop: copied,
+                                    autoplay: copied,
+                                    animationData: animationData,
+                                    rendererSettings: {
+                                        preserveAspectRatio: 'xMidYMid slice'
+                                    }
+                                }} />
+                            </div>
+                            <MagicButton
+                                title={copied ? 'Email copied' : 'Copy my email'}
+                                icon={<IoCopyOutline />}
+                                position='left'
+                                otherClasses='bg-[#161831]'
+                                handleClick={handleCopy}
+                            />
+                        </div>
+                    )}
+
                 </div>
             </div>
         </div>
