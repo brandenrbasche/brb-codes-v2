445f6c0c81b9f805649fb24a45177705757935c0
Branden Basche <brandenbasche@gmail.com>
2024-06-13 12:44:58 -0400

    boilerplate react nextjs

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Spotlight} from "@/components/ui/Spotlight";
+
+const Hero = () => {
+    return (
+        <div className='pb-20 pt-36'>
+            <div>
+                <Spotlight />
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
