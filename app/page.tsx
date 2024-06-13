d48531fe8d7869173bc5f67364381b32c7aa7e31
Branden Basche <brandenbasche@gmail.com>
2024-06-13 14:23:55 -0400

    initial hero completed, working through bento grid cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import {FloatingNav} from "@/components/ui/FloatingNav";
+import {FaHome} from "react-icons/fa";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
    <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
      <div className='max-w-7xl w-full'>
-       <Hero />
+         <FloatingNav navItems={[
+             {name: 'Home', link: '/', icon: <FaHome />}
+         ]} />
+         <Hero />
+         <Grid />
      </div>
    </main>
   );
---
+++ components/Grid.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { gridItems } from '@/data';
+import {BentoGrid, BentoGridItem} from "@/components/ui/BentoGrid";
+
+const Grid = () => {
+    return (
+        <section id='about'>
+            <BentoGrid>
+                {gridItems.map(({ id, title, description, className, img, imgClassName, titleClassName, spareImg}, i) => (
+                    <BentoGridItem
+                        id={id}
+                        key={id}
+                        title={title}
+                        description={description}
+                        className={className}
+                        img={img}
+                        imgClassName={imgClassName}
+                        titleClassName={titleClassName}
+                        spareImg={spareImg}
+                    />
+                ))}
+            </BentoGrid>
+        </section>
+    );
+};
+
+export default Grid;
